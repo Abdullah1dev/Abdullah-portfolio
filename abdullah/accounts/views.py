29d232de3505140de23df5784f42eaba8b1f2693
Abdullah1dev <242677@students.au.edu.pk>
2025-10-09 10:43:38 -0700

    creating login and logout

---
+++ abdullah/accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created! You can now login.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def home(request):
+    return render(request, 'accounts/home.html') 
+
--- abdullah/portfolio/urls.py
+++ abdullah/portfolio/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from accounts import views  # ✅ this imports views.py from your app
 
 urlpatterns = [
+    path('', views.home, name='home'),  # ✅ home view from accounts/views.py
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
