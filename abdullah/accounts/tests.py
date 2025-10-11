775e29d50450b2c1eaf6e8fcd52cfa941da2c237
Abdullah1dev <242677@students.au.edu.pk>
2025-10-11 11:28:28 -0700

    creating test cases

--- abdullah/accounts/tests.py
+++ abdullah/accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class AuthTests(TestCase):
+    
+    def test_register_page_loads(self):
+        response=self.client.get(reverse('register'))
+        self.assertEqual(response.status_code,200)
+        
+        
+    def test_login_page_loads(self):
+        response=self.client.get(reverse('login'))
+        self.assertEqual(response.status_code,200)
+        
+        
+  
+        
