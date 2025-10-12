f8ae5c1d4232b14fcf4a7b21c670b75b8e1562cb
Abdullah1dev <242677@students.au.edu.pk>
2025-10-12 11:59:44 -0700

    create the models.py file completely

--- abdullah/accounts/admin.py
+++ abdullah/accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import CustomUser, Project, Tag
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created')
+    search_fields = ('name',)  
+    ordering = ('-created',)   
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'published', 'updated_at')
+    list_filter = ('published', 'tags')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}  
+    ordering = ('-updated_at',)
+
+     
+
+
+
--- abdullah/accounts/models.py
+++ abdullah/accounts/models.py
@@ -1,9 +1,42 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.text import slugify
 
 class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/', default='default.jpg', blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    demo_link = models.URLField(blank=True, null=True)
+    github_link = models.URLField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Project, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    
 
 
 
